894dc036256c9bebbb5285fecfaac928f41a7a34
Adam Kapuscinski <adam.kap@interia.pl>
2020-05-17 14:03:41 +0200

    Pass functionality to Provider

--- loadbalancer/src/main/java/com/mathesis/loadbalancer/controller/LBController.java
+++ loadbalancer/src/main/java/com/mathesis/loadbalancer/controller/LBController.java
@@ -15,78 +15,28 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
-import java.time.Duration;
-import java.time.Instant;
 import java.util.*;
 
 @RestController
 @RequestMapping("")
 public class LBController {
-    private String[] registeredServices = new String[]{"SERVICEONE", "SERVICETWO", "SERVICETHREE"};
-    private int serviceIdx = 0;
-
-    private ChartDataModel data = new ChartDataModel();
-
-    List<ServiceRequestData> recentRequestData = new ArrayList<>();
     private final LBProvider lbProvider;
-
-    @Autowired
-    private RestTemplate restTemplate;
-    @Autowired
-    private DiscoveryClient discoveryClient;
 
     LBController(LBProvider lbProvider) {
         this.lbProvider = lbProvider;
     }
 
-    @PostConstruct
-    public void init() {
-        this.data.setLabels(new ArrayList<>());
-        List<DataSet> dataSets = new ArrayList<>();
-        for (int i = 0; i < registeredServices.length; i++) {
-            DataSet dataSet = new DataSet();
-            dataSet.setBorderColor(LBProvider.assignBorderColorBasedOnIdx(i));
-            dataSet.setFill(true);
-            dataSet.setLabel(registeredServices[i]);
-            dataSet.setData(new ArrayList<>());
-            dataSets.add(dataSet);
-        }
-        this.data.setDatasets(dataSets);
-    }
     @GetMapping("api/call")
     public ResponseEntity<Object> call() {
         return ResponseEntity.ok().body(new TransmissionModel(callSomeService()));
     }
     @GetMapping
     public String callSomeService() {
-        List<ServiceInstance> instances = this.discoveryClient.getInstances(registeredServices[serviceIdx]);
-        String result = "";
-        if (instances.size() > 0) {
-            ServiceInstance serviceInstance = instances.get(0);
-            Instant start = Instant.now();
-            String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/";
-            System.out.println("calling url: " + url);
-            result = restTemplate.getForObject(url, String.class);
-            Instant finish = Instant.now();
-            assignParamsToMap(start, finish, registeredServices[serviceIdx]);
-        }
-
-        serviceIdx++;
-        if (serviceIdx > 2) {
-            serviceIdx = 0;
-        }
-        return result;
+        return lbProvider.callSomeService();
     }
 
     @GetMapping("api/stats")
     public ChartSetDataModel getStatistics() {
-        return lbProvider.assignDataObject(registeredServices, recentRequestData);
+        return lbProvider.assignDataObject();
     }
-
-    private void assignParamsToMap(Instant start, Instant finish, String registeredService) {
-        long duration = Duration.between(start, finish).toMillis();
-        ServiceRequestData serviceRequestDataObj = new ServiceRequestData(start, registeredService, finish, duration);
-        recentRequestData.add(serviceRequestDataObj);
-    }
-
 }
--- loadbalancer/src/main/java/com/mathesis/loadbalancer/controller/LBProvider.java
+++ loadbalancer/src/main/java/com/mathesis/loadbalancer/controller/LBProvider.java
@@ -4,8 +4,14 @@ import com.mathesis.loadbalancer.domain.ServiceRequestData;
 import com.mathesis.loadbalancer.domain.chart.ChartDataModel;
 import com.mathesis.loadbalancer.domain.chart.ChartSetDataModel;
 import com.mathesis.loadbalancer.domain.chart.DataSet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
+import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
@@ -15,8 +21,34 @@ import java.util.stream.Collectors;
 
 @Service
 public class LBProvider {
+    private String[] registeredServices = new String[]{"SERVICEONE", "SERVICETWO", "SERVICETHREE"};
 
-    ChartSetDataModel assignDataObject(String[] registeredServices, List<ServiceRequestData> recentRequestData) {
+    private ChartDataModel data = new ChartDataModel();
+
+    List<ServiceRequestData> recentRequestData = new ArrayList<>();
+    private int serviceIdx = 0;
+
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @PostConstruct
+    public void init() {
+        this.data.setLabels(new ArrayList<>());
+        List<DataSet> dataSets = new ArrayList<>();
+        for (int i = 0; i < registeredServices.length; i++) {
+            DataSet dataSet = new DataSet();
+            dataSet.setBorderColor(LBProvider.assignBorderColorBasedOnIdx(i));
+            dataSet.setFill(true);
+            dataSet.setLabel(registeredServices[i]);
+            dataSet.setData(new ArrayList<>());
+            dataSets.add(dataSet);
+        }
+        this.data.setDatasets(dataSets);
+    }
+
+    ChartSetDataModel assignDataObject() {
         int timeToShowInMinutes = 5;
         int synchronizationTime = 10;
 
@@ -79,7 +111,7 @@ public class LBProvider {
         return result;
     }
 
-    public static String assignBorderColorBasedOnIdx(int idx) {
+    static String assignBorderColorBasedOnIdx(int idx) {
         switch (idx) {
             case 0: {
                 return "#4bc0c0";
@@ -88,7 +120,33 @@ public class LBProvider {
                 return "#565656";
             }
             default:
-                return "Red";
+                return "red";
         }
+    }
+    String callSomeService() {
+
+        List<ServiceInstance> instances = this.discoveryClient.getInstances(registeredServices[serviceIdx]);
+        String result = "";
+        if (instances.size() > 0) {
+            ServiceInstance serviceInstance = instances.get(0);
+            Instant start = Instant.now();
+            String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/";
+            System.out.println("calling url: " + url);
+            result = restTemplate.getForObject(url, String.class);
+            Instant finish = Instant.now();
+            assignParamsToMap(start, finish, registeredServices[serviceIdx]);
+        }
+
+        serviceIdx++;
+        if (serviceIdx > 2) {
+            serviceIdx = 0;
+        }
+        return result;
+    }
+
+    private void assignParamsToMap(Instant start, Instant finish, String registeredService) {
+        long duration = Duration.between(start, finish).toMillis();
+        ServiceRequestData serviceRequestDataObj = new ServiceRequestData(start, registeredService, finish, duration);
+        recentRequestData.add(serviceRequestDataObj);
     }
 }
