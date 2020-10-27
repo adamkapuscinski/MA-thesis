49697d550999a2b7b9e57538cdcb8d9c2c0abdc0
Adam Kapuscinski <adam.kap@interia.pl>
2020-10-27 20:11:29 +0100

    docker-compose modifications

--- front/src/app/shared/services/load-balancer.service.ts
+++ front/src/app/shared/services/load-balancer.service.ts
@@ -1,20 +1,18 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpResponse} from '@angular/common/http';
-import {SERVER_API_URL} from '../../app.constants';
 import {Observable} from 'rxjs';
-import {IChartDataModel, IChartSetData} from "../model/chart-datatype.model";
+import {IChartSetData} from "../model/chart-datatype.model";
 import {ILoadBalancerConfigModel} from "../model/load-balancer-config.model";
 
 type ChartResponseType = HttpResponse<IChartSetData>;
 
 @Injectable({ providedIn: 'root' })
 export class LoadBalancerService {
-    public resourceUrl = SERVER_API_URL + 'api/stats';
 
     constructor(private http: HttpClient) {}
 
     public stats(): Observable<ChartResponseType> {
-        return this.http.get<IChartSetData>(this.resourceUrl, { observe: 'response' });
+        return this.http.get<IChartSetData>('api/stats', { observe: 'response' });
     }
     call(): Observable<HttpResponse<any>> {
       return this.http.get<any>('api/call', { observe: 'response' });
